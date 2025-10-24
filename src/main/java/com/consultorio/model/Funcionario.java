305ac168ae5b363b1e516b6e27094aceea1a4cfa
NicolasPizato <mohrnicolas@outlook.com>
2025-10-23 22:26:13 -0300

    Criada classe Funcionario com atributos, construtores e getters/setters

---
+++ src/main/java/com/consultorio/model/Funcionario.java
@@ -0,0 +1,44 @@
+package com.consultorio.model;
+
+public class Funcionario {
+
+    private String nome;
+    private int idade;
+    private double salario;
+
+    // Construtor vazio
+    public Funcionario() {
+    }
+
+    // Construtor com todos os campos
+    public Funcionario(String nome, int idade, double salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
