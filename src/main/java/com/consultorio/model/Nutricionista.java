f843ad0a2752a3348cb9a41eef73ff6b208d455a
NicolasPizato <mohrnicolas@outlook.com>
2025-10-23 22:26:45 -0300

    Criada classe Nutricionista herdando de Funcionario, com atributo numeroConsultas

---
+++ src/main/java/com/consultorio/model/Nutricionista.java
@@ -0,0 +1,26 @@
+package com.consultorio.model;
+
+public class Nutricionista extends Funcionario {
+
+    private int numeroConsultas;
+
+    // Construtor vazio
+    public Nutricionista() {
+        super();
+    }
+
+    // Construtor com todos os campos
+    public Nutricionista(String nome, int idade, double salario, int numeroConsultas) {
+        super(nome, idade, salario);
+        this.numeroConsultas = numeroConsultas;
+    }
+
+    // Getter e Setter
+    public int getNumeroConsultas() {
+        return numeroConsultas;
+    }
+
+    public void setNumeroConsultas(int numeroConsultas) {
+        this.numeroConsultas = numeroConsultas;
+    }
+}
